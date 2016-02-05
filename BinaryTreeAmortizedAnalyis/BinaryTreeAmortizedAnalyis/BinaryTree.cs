1b5c27d0c292cdfa306cdffaa9f90b9a958022d7
Peter Ivancis <peter.ivanics@reslink.fi>
2016-02-05 21:49:45 +0200

    Added BinaryTree BinaryTreeTests classes

---
+++ BinaryTreeAmortizedAnalyis/BinaryTreeAmortizedAnalyis/BinaryTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeAmortizedAnalyis
+{
+    /// <summary>
+    /// A class representing a binary tree.
+    /// </summary>
+    public class BinaryTree : IInorderTransversal
+    {
+        #region IInorderTransversal
+        public void inorderFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void inorderNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isInorderTransversalFinished()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ BinaryTreeAmortizedAnalyis/BinaryTreeAmortizedAnalysisTests/BinaryTreeTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinaryTreeAmortizedAnalyis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeAmortizedAnalyis.Tests
+{
+    [TestClass()]
+    public class BinaryTreeTests
+    {
+        #region IInorderTransversalTests
+        [TestMethod()]
+        public void inorderFirstTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void inorderNextTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void isInorderTransversalFinishedTest()
+        {
+            Assert.Fail();
+        }
+        #endregion
+    }
+}
