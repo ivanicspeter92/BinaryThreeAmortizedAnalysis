14fa9591fb31f70f9c9068b9ee480142e42e2f4d
Peter Ivancis <peter.ivanics@reslink.fi>
2016-02-05 22:15:12 +0200

    Added the following passing tests: testInitializingWithDistinctNotOrderedArray() testInitializingWithNotDistinctNotOrderedArray()

--- BinaryTreeAmortizedAnalyis/BinaryTreeAmortizedAnalysisTests/BinaryTreeTests.cs
+++ BinaryTreeAmortizedAnalyis/BinaryTreeAmortizedAnalysisTests/BinaryTreeTests.cs
@@ -11,6 +11,7 @@ namespace BinaryTreeAmortizedAnalyis.Tests
     [TestClass()]
     public class BinaryTreeTests
     {
+        #region Initializer tests
         /// <summary>
         /// Tests the initialization of a BinaryTree with a distinct sequence of integers.
         /// </summary>
@@ -22,7 +23,30 @@ namespace BinaryTreeAmortizedAnalyis.Tests
             CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, tree.nodeValues);
         }
 
-        #region IInorderTransversalTests
+        /// <summary>
+        /// Tests the initialization of a BinaryTree with a distinct but not sequential array of integers.
+        /// </summary>
+        [TestMethod()]
+        public void testInitializingWithDistinctNotOrderedArray()
+        {
+            BinaryTree tree = new BinaryTree(new int[] { 3, 2, 5, 1, 4 });
+
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, tree.nodeValues);
+        }
+
+        /// <summary>
+        /// Tests the initialization of a BinaryTree with a not distinct and not sequential array of integers.
+        /// </summary>
+        [TestMethod()]
+        public void testInitializingWithNotDistinctNotOrderedArray()
+        {
+            BinaryTree tree = new BinaryTree(new int[] { 3, 4, 3, 3, 2, 4, 5, 1, 1, 1 });
+
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, tree.nodeValues);
+        }
+        #endregion
+
+        #region IInorderTransversal tests
         [TestMethod()]
         public void inorderFirstTest()
         {
